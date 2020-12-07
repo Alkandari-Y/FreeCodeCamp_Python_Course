82d5dc33af1b9733076c4bf7fb502158d032c594
[Alkandari-Y] <[yayalkandari@gmail.com]>
2020-12-07 21:56:21 +0300

    completed the first two lessons

---
+++ 1 Introduction- Why Program/sample.py
@@ -0,0 +1,26 @@
+import os
+
+#Took the code and made it run using notes file and os module.
+
+name = input('Enter a file: ')
+cwd_path = os.getcwd()
+doc_path = os.path.join(cwd_path, '1 Introduction- Why Program', name)
+handle = open(doc_path)
+
+counts = dict()
+for line in handle:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+
+print(doc_path)
+print(counts)
+
+bigcount = None
+bigword = None
+
+for word, count in counts.items():
+    if bigcount is None or count > bigcount:
+        bigcount = count
+        bigword = word
+print(bigword, bigcount)
